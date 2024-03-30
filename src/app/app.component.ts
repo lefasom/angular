3edd05fa4ecf1f6742eda63c854de4c8a1ca845c
lefasom <leosombra89@gmail.com>
2024-03-29 21:12:27 -0300

    finalizando tuto de midudev

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from "./user/user.component";
+import { GameComponent } from './game/game.component';
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [RouterOutlet],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//   title = 'angular';
+// }
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserComponent, GameComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular';
+  title = 'Angular';
+  favGame = ""
+  getFavGame(nameGame: string) {
+    this.favGame = nameGame
+  }
 }
